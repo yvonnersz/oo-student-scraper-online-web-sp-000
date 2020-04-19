254c59e15930d7d7622f4c36ce69b02b8ebb437b
Yvonne M Pham <yvonnersz@gmail.com>
2020-04-19 03:12:33 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -6,7 +6,7 @@ class Student
 
   def initialize(student_hash)
 
-    name = self.new(student_hash[:name])
+    self.new(student_hash[:name])
   end
 
   def self.create_from_collection(students_array)
