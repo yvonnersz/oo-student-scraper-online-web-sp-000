2338c07322ce6e1e97aded5b86f7b9976fbe4a25
Yvonne M Pham <yvonnersz@gmail.com>
2020-04-19 03:19:41 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,8 +5,8 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    student_hash.collect do |key, value|
-      value
+    student_hash.collect do |name, location|
+      name
     end
   end
 
