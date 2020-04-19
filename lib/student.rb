e3192afcec477c8ed5cf716699efb19e994eb4b9
Yvonne M Pham <yvonnersz@gmail.com>
2020-04-19 04:18:37 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -18,10 +18,8 @@ class Student
   end
 
   def add_student_attributes(attributes_hash)
-    attributes_hash.each do |key,value|
-      if key == "bio"
-        student[:bio] = value
-      end
+    attributes_hash.each do |key, value|
+      self.send("#{key}=",value)
     end
   end
 
