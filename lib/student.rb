f57fc4e09505fd8efe4d6530f8fc46ec540235f4
Yvonne M Pham <yvonnersz@gmail.com>
2020-04-19 04:12:44 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -12,15 +12,7 @@ class Student
   end
 
   def self.create_from_collection(students_array)
-    # students_array.each do |array|
-    #   students_array.each do |student|
-    #     student.each do |key,value|
-    #       self.send("#{key}=",value)
-    #     end
-    #   end
-    # end
-    # @@all << self
-    binding.pry
+
   end
 
   def add_student_attributes(attributes_hash)
